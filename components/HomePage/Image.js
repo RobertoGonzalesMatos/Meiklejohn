e3e79ac29a2175325018a54382bb6940164e3479
RobertoGonzalesMatos <RobertoGonzalesMatos@users.noreply.github.com>
2024-07-20 17:50:01 +0000

    deploy: 3984b6f8fb2ce002aedf39359c871854a1cd02c5

--- components/HomePage/Image.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-const Image = ({ src, fallback, type = "image/webp", alt, }) => {
-    return (_jsxs("picture", { children: [_jsx("source", { srcSet: src, type: type }), _jsx("img", { src: fallback, alt: alt })] }));
-};
-export default Image;
--- index.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/index.css";
-import App from "./components/App";
-// Finds the root element and starts rendering React there.
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// Render starting with the App components.
-root.render(_jsx(React.StrictMode, { children: _jsx(App, {}) }));
